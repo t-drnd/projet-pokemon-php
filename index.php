a4a589cb1e9ea4c286d33a390ee9234b6284e367
Thomas Durand <durand.thomas765@gmail.com>
2024-11-29 12:20:36 +0100

    test

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Combat Pokemon</title>
+</head>
+<body>
+    <div class="liste_pokemon">
+        <ul>
+            <li><a href="https://pokemondb.net/pokedex/stunfisk"><img src="https://img.pokemondb.net/sprites/black-white/anim/shiny/stunfisk.gif" alt="Stunfisk"></a>Limonde</li>
+            <li><a href="https://pokemondb.net/pokedex/samurott"><img src="https://img.pokemondb.net/sprites/black-white-2/anim/normal/samurott.gif" alt="Samurott"></a>Clamiral</li>
+            <li><a href="https://pokemondb.net/pokedex/jolteon"><img src="https://img.pokemondb.net/sprites/black-white/anim/normal/jolteon.gif" alt="Jolteon"></a>Voltali</li>
+            <li><a href="https://pokemondb.net/pokedex/jynx"><img src="https://img.pokemondb.net/sprites/black-white/anim/normal/jynx.gif" alt="Jynx"></a>Lippoutou</li>
+            <li><a href="https://pokemondb.net/pokedex/weepinbell"><img src="https://img.pokemondb.net/sprites/black-white/anim/normal/weepinbell.gif" alt="Weepinbell"></a>Boustiflor</li>
+            <li><a href="https://pokemondb.net/pokedex/arcanine"><img src="https://img.pokemondb.net/sprites/black-white/anim/normal/arcanine.gif" alt="Arcanine"></a>Arcanin</li>
+        </ul>
+    </div>
+</body>
+</html>
+
 <?php
 
 require_once './class/Pokemon.php';
